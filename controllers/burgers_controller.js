4004a6a3fa30fe03607503212f2d1d785a101a41
tiffihaag <tiffihaag@gmail.com>
2016-10-16 16:23:43 -0400

    close

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,15 +1,16 @@
 var burgers  = require('../models')["burgers"];
-var models  = require('../models/burgers');
 var express = require('express');
 var router = express.Router();
 
-router.get('burgers', function(req, res) {
-	models.burgers.findAll()
+router.get('/', function(req, res) {
+	res.redirect('/burgers');
+});
+
+router.get('/burgers', function(req, res) {
+	burgers.findAll()
 	.then(function(burgers) {
-		res.render('index', {
-      	burgers: burgers
+		res.render('index', {burgers})
     })
-  })
 });
 
 module.exports = router;
